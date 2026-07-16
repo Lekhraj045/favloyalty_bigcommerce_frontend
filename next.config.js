149ae74f372b0fc67de06bf972e49747dd932138
prashantDevDeskmoz <prashantsingh.deskmoz@gmail.com>
2026-07-16 15:25:02 +0530

    minor fix nextConfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // static HTML export
     images: {
-      unoptimized: true, // Required for static export
+      unoptimized: true,
     },
   };
-  
-  module.exports = nextConfig;
+
+module.exports = nextConfig;
   
